f560713ab3e704f449b45aac0f87015a2eaa1186
Aleksey <aleshka.stafeev@bk.ru>
2026-06-06 23:33:45 +0900

    versss14

--- Fitness_club/settings.py
+++ Fitness_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=5)5r3k%k)qt*%er_v2yrnhz@ldn)#$0-(ron*2oyxf1+-vcu!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
